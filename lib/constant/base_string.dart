d26a87e73353451dace07acfd4066894e3972b4a
naufal <askpradana@gmail.com>
2024-09-26 15:33:13 +0700

    fix: base api handler

--- lib/constant/base_string.dart
+++ lib/constant/base_string.dart
@@ -1,5 +1,5 @@
 class BaseString {
   //API
-  static const String baseUrl = 'https://jsonplaceholder.typicode.com';
+  static const String baseUrl = 'https://peanote.adauntukkamu.com/api/v1';
   static const String todos = '/todos';
 }
--- lib/data/baseapi.dart
+++ lib/data/baseapi.dart
@@ -1,7 +1,8 @@
+import 'dart:developer';
+
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
 import 'package:peanote/constant/base_string.dart';
 
 class ApiException implements Exception {
@@ -19,6 +20,10 @@ class BaseApi extends GetxController {
   final String baseUrl = BaseString.baseUrl;
 
   Future<dynamic> _handleResponse(http.Response response) async {
+    log('Response status code: ${response.statusCode}');
+    log('Response body: ${response.body}');
+    // debugPrint('Response headers: ${response.headers}');
+
     try {
       final statusCode = response.statusCode;
       final body = response.body;
@@ -26,14 +31,19 @@ class BaseApi extends GetxController {
       if (statusCode >= 200 && statusCode < 300) {
         return json.decode(body);
       } else if (statusCode >= 400 && statusCode < 500) {
-        throw ApiException('Client error', statusCode: statusCode);
+        throw ApiException('Client error: ${response.reasonPhrase}',
+            statusCode: statusCode);
       } else if (statusCode >= 500) {
-        throw ApiException('Server error', statusCode: statusCode);
+        throw ApiException('Server error: ${response.reasonPhrase}',
+            statusCode: statusCode);
       } else {
-        throw ApiException('Unexpected status code', statusCode: statusCode);
+        throw ApiException('Unexpected status code: ${response.reasonPhrase}',
+            statusCode: statusCode);
       }
-    } on FormatException {
-      throw ApiException('Invalid response format');
+    } on FormatException catch (e) {
+      throw ApiException(
+        'Invalid response format: $e',
+      );
     }
   }
 
@@ -42,30 +52,46 @@ class BaseApi extends GetxController {
     try {
       final response = await requestFunction();
       return await _handleResponse(response);
-    } on http.ClientException {
-      throw ApiException('Network error or invalid URL');
-    } on ApiException {
+    } on http.ClientException catch (e) {
+      // log('ClientException: $e');
+      throw ApiException('Network error or invalid URL: ${e.message}');
+    } on ApiException catch (e) {
+      log('ApiException: $e');
       rethrow;
     } catch (e) {
+      // log('Unexpected error: $e');
       throw ApiException('Unexpected error: $e');
     }
   }
 
   Future<dynamic> get(String endpoint, {Map<String, String>? headers}) async {
+    log('GET request to: $baseUrl$endpoint');
+    // debugPrint('Headers: $headers');
+
     return _makeRequest(
       () => http.get(
         Uri.parse('$baseUrl$endpoint'),
-        headers: headers,
+        headers: {
+          'Content-Type': 'application/json',
+          ...?headers,
+        },
       ),
     );
   }
 
   Future<dynamic> post(String endpoint,
       {Map<String, String>? headers, dynamic body}) async {
+    log('POST request to: $baseUrl$endpoint');
+    // debugPrint('Headers: $headers');
+    log('Body: $body');
+
     return _makeRequest(
       () => http.post(
         Uri.parse('$baseUrl$endpoint'),
-        headers: headers,
+        headers: {
+          'Content-Type': 'application/json',
+          ...?headers,
+        },
         body: json.encode(body),
       ),
     );
